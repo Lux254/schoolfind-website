86ecfeb3baf8c699fbf09a8fbba1b186945c8d0f
Shadrack Ohungo <shadrackohungo@gmail.com>
2025-09-16 23:38:13 +0300

    Initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.querySelector('.nav-toggle');
+const overlay = document.querySelector('.nav-overlay');
+
+toggleBtn.addEventListener('click', () => {
+    overlay.classList.toggle('active');
+
+    // Change icon ☰ <-> ✖
+    if (overlay.classList.contains('active')) {
+        toggleBtn.textContent = '✖';
+    } else {
+        toggleBtn.textContent = '☰';
+    }
+});
